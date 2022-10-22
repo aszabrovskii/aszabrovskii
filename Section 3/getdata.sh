7b357c9b33e037bf2514c5c82cd9301d65aeaa80
aszabrovskii <116036123+aszabrovskii@users.noreply.github.com>
2022-10-23 01:38:44 +0300

    Code files uploaded

---
+++ Section 3/getdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Shebang
+
+# Скрипт предназначен для извлечения данных из графы title и вывода в stdout в алфавитном порядке
+
+function get_list () {
+
+for as in $(egrep -i 'title' $lib | head -n 2 | awk 'BEGIN{FS=":"} {print $2}'| cut -d'"' -f2)
+do 
+echo $as
+done | sort
+}
+
+lib=(./lib.json)
+get_list
